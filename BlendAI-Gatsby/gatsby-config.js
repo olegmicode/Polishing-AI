ef9b93db0be5e306ddaa327a30bfc48e130ddeee
topcodewizard007 <mykytaya007@gmail.com>
2022-06-23 04:45:53 +0300

    init project

---
+++ BlendAI-Gatsby/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    title: `blend-ai`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
