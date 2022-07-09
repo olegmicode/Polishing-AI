948ceb1935913039b33e412316a5eb7b638b42f1
topcodewizard007 <mykytaya007@gmail.com>
2022-07-09 08:04:11 +0300

    integrated product feed

--- BlendAI-Gatsby/src/components/home/services.js
+++ BlendAI-Gatsby/src/components/home/services.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react"
-import BlockContent from "@sanity/block-content-to-react";
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-import ShopifyImg from "../../images/tild3763-6263-4138-b437-396561643236__shopify.png"
-import MarapostImg from "../../images/tild3162-6339-4337-a363-636165623431__marapost.png"
-import WoocommerceImg from "../../images/tild3561-3866-4163-b466-303031373737__woocommerce.png"
+// import ShopifyImg from "../../images/tild3763-6263-4138-b437-396561643236__shopify.png"
+// import MarapostImg from "../../images/tild3162-6339-4337-a363-636165623431__marapost.png"
+// import WoocommerceImg from "../../images/tild3561-3866-4163-b466-303031373737__woocommerce.png"
 import "./services.css"
 
 const Services = ({ data }) => {
