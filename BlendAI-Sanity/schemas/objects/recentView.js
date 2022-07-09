5c80d4d83a8d095d8363a3fb7844fc1088dd0854
topcodewizard007 <mykytaya007@gmail.com>
2022-07-09 12:57:00 +0300

    recent views

---
+++ BlendAI-Sanity/schemas/objects/recentView.js
@@ -0,0 +1,30 @@
+// recentViews.js
+
+export default {
+    name: "recentView",
+    type: "object",
+    title: "One Recent View",
+    fields: [
+        {
+            name: "recommendation",
+            title: "Recommendation",
+            type: "string",
+        },
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+        },
+        {
+            name: "siteName",
+            title: "Site Name",
+            type: "string",
+        },
+        {
+            name: "siteLink",
+            title: "Site Link",
+            type: "string",
+        }
+
+    ]
+}
