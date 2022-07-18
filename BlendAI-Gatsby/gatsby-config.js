edf74cac209c53e3e6d10026e9cab60de73137d9
topcodewizard007 <mykytaya007@gmail.com>
2022-07-18 12:15:11 +0300

    add favicon

--- BlendAI-Gatsby/gatsby-config.js
+++ BlendAI-Gatsby/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `blend-ai`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://blend-ai.com/`
   },
   plugins: [
     {
@@ -15,7 +15,23 @@ module.exports = {
       }
     },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+    "gatsby-plugin-sass", "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        // name: `gatsby-starter-default`,
+        // short_name: `starter`,
+        // start_url: `/`,
+        // background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        // display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
    
   ]
 };
