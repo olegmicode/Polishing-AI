7a79a9f568a75d9dfff27533fe62e00d90567ba0
topcodewizard007 <mykytaya007@gmail.com>
2022-07-20 15:56:35 +0300

    fixed head key issue

--- BlendAI-Gatsby/gatsby-ssr.js
+++ BlendAI-Gatsby/gatsby-ssr.js
@@ -3,6 +3,7 @@ const React = require("react")
 export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
     setHeadComponents([
         <script
+            key="my-script"
             src="https://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossOrigin="anonymous"
